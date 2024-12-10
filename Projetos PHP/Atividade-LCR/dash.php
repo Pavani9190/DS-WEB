f6b562dfab833b9e7ac468562ce3391042e94c19
Pavani9190 <gutopavani439@gmail.com>
2024-12-10 08:49:49 -0300

    Atividade final

--- Projetos PHP/Atividade-LCR/dash.php
+++ Projetos PHP/Atividade-LCR/dash.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cadastrar_cliente']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Cadastro</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
